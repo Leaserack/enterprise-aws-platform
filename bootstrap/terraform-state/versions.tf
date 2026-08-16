e0ae4f2d124ebe274ae99c6272ae91df0f37e2b8
Karan Sawale <karannsawale@gmail.com>
2026-08-17 00:12:02 +0530

    chore: standardize terraform versions and workflows

--- bootstrap/terraform-state/versions.tf
+++ bootstrap/terraform-state/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.10.0, < 2.0.0"
+  required_version = "= 1.15.5"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.57"
+      version = "= 6.57.1"
     }
   }
 }
